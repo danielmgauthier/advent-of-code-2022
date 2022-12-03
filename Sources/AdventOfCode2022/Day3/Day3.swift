a56b9252ea3d5946da01ae9f544709d495dfcc2b
Daniel Gauthier <daniel.m.gauthier@icloud.com>
2022-12-03 11:50:30 -0500

    Add day 3 solution.

--- Sources/AdventOfCode2022/Day3/Day3.swift
+++ Sources/AdventOfCode2022/Day3/Day3.swift
@@ -1,11 +1,35 @@
 import Foundation
 
 struct Day3: Runnable {
+    let priorityArray = Array("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    let rucksacks = Self.input.split(separator: "\n")
+    
     func partOne() -> String {
-        ""
+        var total = 0
+        rucksacks.forEach {
+            let comp1 = $0.prefix($0.count / 2)
+            let comp2 = $0.suffix($0.count / 2)
+            let seen = Set(comp1)
+            let commonCharacter = comp2.first { seen.contains($0) }!
+            total += priority(for: commonCharacter)
+        }
+        return total.description
     }
     
     func partTwo() -> String {
-        ""
+        let elfGroups = rucksacks.chunk(by: 3)
+        var total = 0
+        elfGroups.forEach {
+            let seen1 = Set($0[0])
+            let seen2 = Set($0[1])
+            let commonCharacter = $0[2].first { seen1.contains($0) && seen2.contains($0) }!
+            total += priority(for: commonCharacter)
+        }
+        
+        return total.description
+    }
+    
+    private func priority(for character: Character) -> Int {
+        priorityArray.firstIndex(of: character)! + 1
     }
 }
--- Sources/AdventOfCode2022/Extensions.swift
+++ Sources/AdventOfCode2022/Extensions.swift
@@ -3,3 +3,15 @@ import Foundation
 extension Array where Element == Int {
     var sum: Int { self.reduce(0, +) }
 }
+
+extension Array {
+    func chunk(by size: Int) -> [Self] {
+        self.enumerated().reduce(into: [Self]()) {
+            if $1.offset % size == 0 {
+                $0.append([$1.element])
+            } else {
+                $0[$1.offset / size].append($1.element)
+            }
+        }
+    }
+}
